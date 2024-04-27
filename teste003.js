d4aff19a5d0752a41ead09e9cca931c5c5ec770b
CaiqueSantosOC <caiquesantosmda@gmail.com>
2024-04-26 23:05:28 -0300

    terminando exercicios

---
+++ teste003.js
@@ -0,0 +1,12 @@
+let vel = 40
+console.log (`A velocidade do seu carro é: ${vel}km/h.`)
+if (vel > 60) {
+    console.log ('Você ultrapassou a velocidade permitida! MULTADO!') } 
+else {
+    console.log ('Você está dirigindo muito bem!') }
+console.log ('Dirija sempre usando o cinto de segurança!')
+
+    
+
+    //IF E ELSE SÃO UTILIZADOS PARA REALIZAR CONDIÇÕES SIMPLES DENTRO DE ARQUIVOS EM JS - JAVASCRIPT.  
+
