01d266852191a80c4b574c0777cc76e754f3b9d8
Caique Santos <166532520+CaiqueSantosOC@users.noreply.github.com>
2024-04-29 08:54:41 -0300

    esta fluindo agora

---
+++ teste005.js
@@ -0,0 +1,7 @@
+let pais = 'EUA'
+console.log (`Atualmente você está morando no ${pais}.`)
+if (pais == 'Brasil') {
+    console.log ('Você não é estrangeiro.')
+} else {
+    console.log ('Voçê é estrangeiro.')
+}
