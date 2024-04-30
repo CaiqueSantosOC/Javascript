42990ed2f921500a111dbec953bd56e7fbca3493
CaiqueSantosOC <caiquesantosmda@gmail.com>
2024-04-30 19:37:05 -0300

    projeto em andamento

--- Exercicios/Simulador-Idade/script.js
+++ Exercicios/Simulador-Idade/script.js
@@ -1,1 +1,13 @@
-window.alert('Olá!')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert(' [ERRO] Por favor, verifique os dados e tente novamente!')
+    } 
+    else {
+        
+    }
+    
+}
