61d9c98702e1b1ba1058dfbd05e4a7147e4476d2
Caique Santos <166532520+CaiqueSantosOC@users.noreply.github.com>
2024-05-02 12:57:37 -0300

    update project

--- Exercicios/Simulador-Idade/script.js
+++ Exercicios/Simulador-Idade/script.js
@@ -4,10 +4,16 @@ function verificar(){
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     if (fano.value.length == 0 || fano.value > ano) {
-        window.alert(' [ERRO] Por favor, verifique os dados e tente novamente!')
-    } 
-    else {fano.value
-        
+        window.alert(' [ERRO] Por favor, verifique os dados e tente novamente!') 
+    } else { 
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem!'
+        } else if (fsex[1].checked) {
+            genero = 'Mulher!'
+        }
+        res.innerHTML = `Detectamos: ${genero} com ${idade} anos.`
     }
-    
 }
