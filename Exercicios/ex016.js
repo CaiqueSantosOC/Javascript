d20e26a2a0d7b68446865f48739f47ab8567837d
Caique Santos <166532520+CaiqueSantosOC@users.noreply.github.com>
2024-05-03 15:48:24 -0300

    contador em js

---
+++ Exercicios/ex016.js
@@ -0,0 +1,5 @@
+//Para repetições em javascript utilizamos o comando 
+//WHILE, DO WHILE E FOR. (exemplo abaxo.)
+for (let c = 1; c <= 5; c++) {
+    console.log(`O contador esta marcando passo ${c}`)
+}
