8e69ea965781f5a6e3156a19be513b4353083588
CaiqueSantosOC <caiquesantosmda@gmail.com>
2024-05-02 23:02:08 -0300

    projeto concluído

--- Exercicios/Simulador-Idade/script.js
+++ Exercicios/Simulador-Idade/script.js
@@ -15,13 +15,17 @@ function verificar(){
         img.setAttribute('id', 'foto')   
         if (fsex[0].checked) {
             genero = 'Homem,'
-            if (idade >= 0 & idade <= 10) {
-                //ciança
-                img.setAttribute('src', 'menino.png')
-            } else if (idade <= 21) {
-                //jovem
+            if (idade >= 0 & idade <= 4) {
+                //bebê garoto
+                img.setAttribute('src', 'baby.b.png')
+            } else if (idade < 13) {
+                //criança menino
+                img.setAttribute('src','menino.png')
+            }
+             else if (idade <= 21) {
+                //jovem rapaz
                 img.setAttribute('src', 'jovem.png')
-            } else if (idade <50 ) {
+            } else if (idade < 50 ) {
                 //adulto
                 img.setAttribute('src', 'homem.png')
             } else if (idade > 51) {
@@ -30,18 +34,26 @@ function verificar(){
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher,'
-            if (idade >= 0 & idade < 10) {
-                //criança
-            } else if (idade < 21) {
-                //jovem
+            if (idade >= 0 & idade <= 4) {
+                //bebê moça
+                img.setAttribute('src', 'baby.g.png') 
+            } else if (idade < 13) {
+                //menina
+                img.setAttribute('src', 'menina.png')
+            } else if (idade <= 21) {
+                //jovem mulher
+                img.setAttribute('src', 'jovem-f.png')
             } else if (idade < 50) {
-                //adulto 
+                //adulta
+                img.setAttribute('src', 'mulher.png') 
             }  else if (idade > 51) {
-                //idoso
+                //idosa
+                img.setAttribute('src', 'velha.png')
             }  
             
         }
         res.innerHTML = `Detectamos: ${genero} com ${idade} anos.`
         res.appendChild(img)
-    }
+    }    
+    
 }
