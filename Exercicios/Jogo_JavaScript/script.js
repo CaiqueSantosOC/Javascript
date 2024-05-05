ba6b9dc9bd7480a7a93f02c06061d8fc8d18bb4f
CaiqueSantosOC <caiquesantosmda@gmail.com>
2024-05-05 15:10:15 -0300

    update imagens jogos

--- Exercicios/Jogo_JavaScript/script.js
+++ Exercicios/Jogo_JavaScript/script.js
@@ -1,13 +1,31 @@
 
 const sasuke = document.querySelector('.sasuke')
+const sharingan = document.querySelector('.sharingan')
 
 const jump = () => {
     sasuke.classList.add('jump')
 
-    setTimeout(() => {
+    setTimeout(() => {  
         sasuke.classList.remove('jump')
 
     }, 500)
 }
+
+const loop = setInterval(() => {
+    const sharinganPosition = sharingan.offsetLeft
+    const sasukePosition = +window.getComputedStyle(sasuke).bottom.replace('px', '')
+
+    console.log(sasukePosition)
+
+    if (sharinganPosition <= 110 && sharinganPosition > 0 && sasukePosition < 80) {
+
+        sharingan.style.animation = 'none'
+        sharingan.style.left = `${sharinganPosition}px`
+
+        sasuke.style.animation = 'none'
+        sasuke.style.left = `${sasukePosition}px`
+    }
+
+}, 10) 
 
 document.addEventListener('keydown', jump)
